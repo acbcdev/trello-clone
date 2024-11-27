d4bfee3154ec847e7a12a50fe746ca503d4186c4
ACBC <acbc.dev@gmail.com>
2024-11-27 12:25:22 -0500

    fix padding en on inputs

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -8,27 +8,36 @@ import type { Column } from '@app/types/data';
   standalone: true,
   imports: [ReactiveFormsModule, AddfocusDirective],
   templateUrl: './add-item.component.html',
-  styleUrl: './add-item.component.css'
 })
 export class AddItemComponent {
-  columnId = input.required<Column['id']>()
-  private dataService = inject(DataService)
-  createInput = new FormControl('', { validators: [Validators.required, Validators.nullValidator] })
+  columnId = input.required<Column['id']>();
+  private dataService = inject(DataService);
+  createInput = new FormControl('', {
+    validators: [Validators.required, Validators.nullValidator],
+  });
   addItem(columnId: Column['id']) {
-    const text = this.createInput.value
-    const validteInput = text?.trim() === '' || !text
+    const text = this.createInput.value;
+    const validteInput = text?.trim() === '' || !text;
 
-    if (this.createInput.invalid || validteInput) return
-    this.dataService.addItem(columnId, { id: Math.floor(Math.random() * 120000), name: text, description: '', tags: [], isEditing: false, isOpen: false })
-    this.createInput.reset()
+    if (this.createInput.invalid || validteInput) return;
+    this.dataService.addItem(columnId, {
+      id: Math.floor(Math.random() * 120000),
+      name: text,
+      description: '',
+      tags: [],
+      isEditing: false,
+      isOpen: false,
+    });
+    this.createInput.reset();
   }
   cancelAddition(columnId: Column['id']) {
-    this.dataService.columns.update((prev) => prev.map((column) => (
-      column.id === columnId
-        ? { ...column, isAdding: !column.isAdding }
-        :
-        column
-    )))
-    this.createInput.reset()
+    this.dataService.columns.update((prev) =>
+      prev.map((column) =>
+        column.id === columnId
+          ? { ...column, isAdding: !column.isAdding }
+          : column
+      )
+    );
+    this.createInput.reset();
   }
 }
