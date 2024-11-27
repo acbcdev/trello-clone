8f9e693f639209fc3d3620c57b43dd4ad6cd76e5
ACBC <acbc.dev@gmail.com>
2024-11-27 11:40:16 -0500

    fix header styles

--- src/app/components/column-header/column-header.component.ts
+++ src/app/components/column-header/column-header.component.ts
@@ -1,47 +1,47 @@
-import { Component, inject, input, } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { AddfocusDirective } from '@app/directive/addfocus.directive';
 import { DataService } from '@app/services/data.service';
 import type { Column } from '@app/types/data';
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
-import { faXmark, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faXmark, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-column-header',
   standalone: true,
   imports: [ReactiveFormsModule, FontAwesomeModule, AddfocusDirective],
   templateUrl: './column-header.component.html',
-  styleUrl: './column-header.component.css'
 })
 export class ColumnHeaderComponent {
-  faXmark = faXmark
-  faPenToSquare = faPenToSquare
-  column = input.required<Column>()
+  faXmark = faXmark;
+  faPenToSquare = faPenToSquare;
+  column = input.required<Column>();
 
-  editColumnInput = new FormControl('')
-  private dataService = inject(DataService)
+  editColumnInput = new FormControl('');
+  private dataService = inject(DataService);
   editNameColumn(columnId: Column['id']) {
-    const title = this.editColumnInput.value
-    const validteInput = title?.trim() === ''
-    if (this.editColumnInput.invalid || validteInput) return
-    this.dataService.editColumn(columnId, title ?? '')
-    this.editColumnInput.reset()
-    this.editColumnHandler(columnId)
+    const title = this.editColumnInput.value;
+    const validteInput = title?.trim() === '';
+    if (this.editColumnInput.invalid || validteInput) return;
+    this.dataService.editColumn(columnId, title ?? '');
+    this.editColumnInput.reset();
+    this.editColumnHandler(columnId);
   }
 
   editColumnHandler(columnId: Column['id']) {
-    this.editColumnInput.setValue(this.column().title)
-    this.dataService.columns.update((prev) => prev.map((column) => {
-      if (column.id === columnId) {
-        return { ...column, isEditing: !column.isEditing }
-      }
-      return { ...column, isEditing: false }
-    }))
+    this.editColumnInput.setValue(this.column().title);
+    this.dataService.columns.update((prev) =>
+      prev.map((column) => {
+        if (column.id === columnId) {
+          return { ...column, isEditing: !column.isEditing };
+        }
+        return { ...column, isEditing: false };
+      })
+    );
   }
 
   removeColumn(id: Column['id']) {
-    this.dataService.columns.update((prev) => prev.filter((column) => (
-      column.id !== id
-    )))
+    this.dataService.columns.update((prev) =>
+      prev.filter((column) => column.id !== id)
+    );
   }
-
 }
--- src/app/components/columns/columns.component.ts
+++ src/app/components/columns/columns.component.ts
@@ -1,5 +1,17 @@
-import { Component, Injector, effect, inject, signal, PLATFORM_ID } from '@angular/core';
-import { type CdkDragDrop, DragDropModule, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  Component,
+  Injector,
+  effect,
+  inject,
+  signal,
+  PLATFORM_ID,
+} from '@angular/core';
+import {
+  type CdkDragDrop,
+  DragDropModule,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { DataService } from '@app/services/data.service';
 import type { Column, Row, TDialog } from '@app/types/data';
 import { ColumnHeaderComponent } from '../column-header/column-header.component';
@@ -7,14 +19,21 @@ import { DialogRowsComponent } from '../dialog-rows/dialog-rows.component';
 import { AddItemComponent } from '../add-item/add-item.component';
 import { ItemComponent } from '../item/item.component';
 import { AddColumnComponent } from '../add-column/add-column.component';
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
-import { faXmark, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faXmark, faPlus } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-columns',
   standalone: true,
-  imports: [DragDropModule, ColumnHeaderComponent, DialogRowsComponent, AddItemComponent, ItemComponent, AddColumnComponent, FontAwesomeModule],
+  imports: [
+    DragDropModule,
+    ColumnHeaderComponent,
+    DialogRowsComponent,
+    AddItemComponent,
+    ItemComponent,
+    AddColumnComponent,
+    FontAwesomeModule,
+  ],
   templateUrl: './columns.component.html',
-  styleUrl: './columns.component.css',
   styles: `
   .cdk-drop-list-dragging .cdk-drag {
       transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
@@ -26,30 +45,30 @@ import { faXmark, faPlus } from '@fortawesome/free-solid-svg-icons'
   .cdk-drag-animating {
       transition: transform 300ms cubic-bezier(0, 0, 0.2, 1);
   }
-`
+`,
 })
 export class ColumnsComponent {
-  faXmark = faXmark
-  faPlus = faPlus
-  dataService = inject(DataService)
-  columns = this.dataService.columns
+  faXmark = faXmark;
+  faPlus = faPlus;
+  dataService = inject(DataService);
+  columns = this.dataService.columns;
 
   title = 'trello-clone';
-  itemDialog = signal<TDialog | null>(null)
+  itemDialog = signal<TDialog | null>(null);
 
   drop(event: CdkDragDrop<Row[]>) {
     transferArrayItem(
       event.previousContainer.data,
       event.container.data,
       event.previousIndex,
-      event.currentIndex,
+      event.currentIndex
     );
   }
   dropColumn(event: CdkDragDrop<Column[]>) {
     moveItemInArray(
       event.container.data,
       event.previousIndex,
-      event.currentIndex,
+      event.currentIndex
     );
   }
   initColumns() {
@@ -58,20 +77,20 @@ export class ColumnsComponent {
     else this.dataService.initColumns();
     this.trackColumns();
   }
-  platform = inject(PLATFORM_ID)
-  injector = inject(Injector)
+  platform = inject(PLATFORM_ID);
+  injector = inject(Injector);
   ngOnInit() {
-    console.log(this.platform)
+    console.log(this.platform);
     if (this.platform === 'web') {
-      this.initColumns()
+      this.initColumns();
     }
 
-    this.initColumns()
+    this.initColumns();
   }
   trackColumns() {
     effect(
       () => {
-        const columns = this.dataService.columns()
+        const columns = this.dataService.columns();
         localStorage.setItem('columns', JSON.stringify(columns));
       },
       { injector: this.injector }
@@ -79,44 +98,59 @@ export class ColumnsComponent {
   }
 
   toggleAdditionState(columnId: Column['id']) {
-    this.dataService.columns.update((prev) => prev.map((column) => (
-      column.id === columnId
-        ? { ...column, isAdding: !column.isAdding }
-        :
-        { ...column, isAdding: false }
-    )))
+    this.dataService.columns.update((prev) =>
+      prev.map((column) =>
+        column.id === columnId
+          ? { ...column, isAdding: !column.isAdding }
+          : { ...column, isAdding: false }
+      )
+    );
   }
   removeItem(columnId: Column['id'], RowId: Row['id']) {
-    this.dataService.columns.update((prev) => prev.map((column) => (
-      column.id === columnId
-        ? { ...column, items: column.items.filter((item) => item.id !== RowId) }
-        : column
-    )))
+    this.dataService.columns.update((prev) =>
+      prev.map((column) =>
+        column.id === columnId
+          ? {
+              ...column,
+              items: column.items.filter((item) => item.id !== RowId),
+            }
+          : column
+      )
+    );
   }
 
   toggleOpen(columnId: Column['id'], RowId: Row['id']) {
-    this.dataService.columns.update((prev) => prev.map((column) => (
-      column.id === columnId
-        ? {
-          ...column, items: column.items.map((item) => {
-            if (item.id === RowId) {
-              const itemToogle = { ...item, isOpen: !item.isOpen, list: column.title, ColumnId: columnId };
-              this.itemDialog.set(itemToogle)
-              console.log(this.itemDialog()?.isOpen)
-              return itemToogle
+    this.dataService.columns.update((prev) =>
+      prev.map((column) =>
+        column.id === columnId
+          ? {
+              ...column,
+              items: column.items.map((item) => {
+                if (item.id === RowId) {
+                  const itemToogle = {
+                    ...item,
+                    isOpen: !item.isOpen,
+                    list: column.title,
+                    ColumnId: columnId,
+                  };
+                  this.itemDialog.set(itemToogle);
+                  console.log(this.itemDialog()?.isOpen);
+                  return itemToogle;
+                }
+                return { ...item, isOpen: false };
+              }),
             }
-            return { ...item, isOpen: false }
-          }
-          )
-        }
-        : column
-    )))
+          : column
+      )
+    );
   }
   closeModal() {
-    this.dataService.columns.update((prev) => prev.map((column) => (
-      { ...column, items: column.items.map(item => ({ ...item, isOpen: false })) }
-    )))
-    this.itemDialog.set(null)
+    this.dataService.columns.update((prev) =>
+      prev.map((column) => ({
+        ...column,
+        items: column.items.map((item) => ({ ...item, isOpen: false })),
+      }))
+    );
+    this.itemDialog.set(null);
   }
-
 }
